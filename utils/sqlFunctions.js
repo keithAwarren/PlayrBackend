9e6f52e97ae757e93474f2638007dbe77e77113e
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2025-02-12 16:07:58 -0330

    deployment testing

--- utils/sqlFunctions.js
+++ utils/sqlFunctions.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "test-password",
-  database: "playr_backend",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
